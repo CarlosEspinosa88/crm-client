c6cc371bf7dee52f4f60151e6f1ce4bcad4dbfc6
Carlos Espinosa <carloespinosa88@gmail.com>
2023-08-15 16:04:20 -0500

    add new apollo config authorization

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,56 @@
 import Image from 'next/image'
 import Layout from '@/components/Layout'
+import { useQuery, gql } from '@apollo/client'
+
+const OBTENER_CLIENTES = gql`
+  query ObtenerClientesVendedor {
+    obtenerClientesVendedor {
+      id
+      nombre
+      apellido
+      empresa
+      vendedor
+    }
+  }
+`
 
 export default function Home() {
+  const { data, loading, error } = useQuery(OBTENER_CLIENTES)
+
   return (
     <div>
       <Layout>
-        <p className='text-slate-700'>Hola desde Clientes</p>
+        {loading ? (
+          <p className="text-gray-800 font-light" >Loading....</p>
+        ) : (
+          <>
+            <h1 className='text-slate-700 text-2xl font-light'>Clientes</h1>
+            <table className='table-auto shadow-md mt-10 w-full w-lg'>
+              <thead className='bg-slate-800'>
+                <tr className=' text-white'>
+                  <th className='w-1/5 py-2'>Nombre</th>
+                  <th className='w-1/5 py-2'>Empresa</th>
+                  <th className='w-1/5 py-2'>Vendedor</th>
+                </tr>
+              </thead>
+              <tbody className='bg-white'>
+                {data?.obtenerClientesVendedor?.map(cliente => (
+                  <tr key={cliente.id}>
+                    <td className='border px-4 py-2 text-gray-800'>
+                      {cliente.name} {cliente.apellido}
+                    </td>
+                      <td className='border px-4 py-2 text-gray-800'>
+                      {cliente.empresa}
+                    </td>
+                    <td className='border px-4 py-2 text-gray-800'>
+                      {cliente.vendedor}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
       </Layout>
   </div>
   )
